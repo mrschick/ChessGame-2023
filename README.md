cad06cd8e2ac92f5389c6bb73e0e281002801abb
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2023-04-12 13:59:40 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Programming Project 2022-23
+
+## The Goal
+Creating a Chess Game from scratch.
+
+## Team Members
+* Sharjeel Mahmood
+* Ridvan Plluzhina
+* Fabio Schick
---
+++ src/ChessGUI.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ChessGUI extends Application {
+	public void start(Stage primaryStage) {
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
