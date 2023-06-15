b261874c91728041238018174750470da511d8f5
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2023-06-15 17:36:53 +0200

    Divided GUI into 2 scenes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,55 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public void start(Stage primaryStage) {
+        // Menu scene setup, with short explanation of the game and settings.
+        FlowPane menuPane = new FlowPane(Orientation.VERTICAL);
+        Scene menuScene = new Scene(menuPane, 1150, 700);
+        Label menutxt = new Label("This is a chess game\n"
+                            +"\n"
+                            +"\n"
+                            +"\n"
+                            +"\n");
+        Button start = new Button("Start Game");
+        menuPane.getChildren().addAll(menutxt, start);
+
+        // Game scene setup, with chessboard and current game info.
+        FlowPane gamePane = new FlowPane();
+        Scene gameScene = new Scene(gamePane, 1150, 700);
+        start.setOnMouseClicked(evt -> {
+            primaryStage.setScene(gameScene);
+        });
         Board board = new Board();
-        Scene scene = new Scene(board, 675, 730);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Chess 2D");
+        FlowPane gameInfo = new FlowPane(Orientation.VERTICAL);
+        gameInfo.setPadding(new Insets(30, 20, 30, 20));
+        gameInfo.setVgap(10);
+
+        FlowPane wPlayerInfo = new FlowPane();
+        wPlayerInfo.setVgap(10);
+        wPlayerInfo.setPadding(new Insets(30, 20, 30, 20));
+        wPlayerInfo.setStyle("-fx-border-color: white");
+        Label wInfo = new Label("White player:\n\n");
+        wPlayerInfo.getChildren().addAll(wInfo);
+
+        FlowPane bPlayerInfo = new FlowPane();
+        bPlayerInfo.setVgap(10);
+        bPlayerInfo.setPadding(new Insets(30, 20, 30, 20));
+        wPlayerInfo.setStyle("-fx-border-color: black");
+        Label bInfo = new Label("Black Player:\n\n");
+        bPlayerInfo.getChildren().addAll(bInfo);
+
+        gameInfo.getChildren().addAll(wPlayerInfo, bPlayerInfo);
+        gamePane.getChildren().addAll(board, gameInfo);
+
+        primaryStage.setScene(menuScene);
+        primaryStage.setTitle("ChessGame");
         primaryStage.show();
     }
 
