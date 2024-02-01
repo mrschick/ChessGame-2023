639506c0b0ebb214461e52109910ad5ab1352eff
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2024-02-01 16:37:05 +0100

    Alter third scheme's windowBG Color

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
             Color.rgb(118, 150, 86),
             Color.rgb(238, 238, 210),
             Color.rgb(185, 144, 24),
-            Color.rgb(200, 200, 200)
+            Color.rgb(255, 245, 200)
         );
 
         FlowPane colorSchemes = new FlowPane();
